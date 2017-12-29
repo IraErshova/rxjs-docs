2d96118b0241b35921a47e481f0c66b2e881d251
Tracy Lee <tracy@thisdot.co>
2017-12-29 08:59:47 -0800

    Revert "feat(teams page): Design a teams page"

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { TeamService } from './team.service';
-import { ITeam } from './team.models';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit {
-  team$: Observable<ITeam>;
-
-  constructor(
-    private service: TeamService
-  ) {}
-
-  ngOnInit() {
-    this.team$ = this.service.getTeam();
-  }
+export class TeamComponent {
+  constructor() {}
 }
--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -1,27 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import {
-  MatCardModule,
-  MatButtonModule,
-  MatIconModule,
-} from '@angular/material';
-
-import { TeamRoutingModule } from './team-routing.module';
 import { TeamComponent } from './team.component';
-import { TeamService } from './team.service';
-import { MemberComponent } from './member.component';
-import { SocialSharingComponent } from './social-sharing/social-sharing.component';
+import { TeamRoutingModule } from './team-routing.module';
 
 @NgModule({
-  imports: [
-    TeamRoutingModule,
-    CommonModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule
-  ],
-  providers: [TeamService],
-  declarations: [TeamComponent, MemberComponent, SocialSharingComponent]
+  imports: [TeamRoutingModule],
+  declarations: [TeamComponent]
 })
 export class TeamModule {}
